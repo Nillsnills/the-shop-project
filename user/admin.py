599e7f8eea0805dd81ff25a0bf672558bb7a793d
nilufar <nills81r@gmail.com>
2024-10-23 21:29:37 +0330

    user model registered in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'phone_number', 'is_superuser']
+    ordering = ['date_joined']
+    search_fields = ['username', 'phone_number']
+
