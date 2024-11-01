c08c788a3730f3177bd2f5f6dcff8c409a7a7eee
nilufar <nills81r@gmail.com>
2024-11-02 01:55:04 +0330

    shop models registered in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Category, Product, Order, OrderItem
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'created_at']
+    search_fields = ['name', 'category', 'description']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status', 'order_date']
+    search_fields = ['user', 'order_date']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'order']
+    search_fields = ['product', 'order']
+
